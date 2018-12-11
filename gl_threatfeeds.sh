12dd13215ec1909655732338061d6def1f5bfdf3
ionstorm <defconoii@gmail.com>
2018-12-11 09:51:13 -0500

    add emerging threats

--- gl_threatfeeds.sh
+++ gl_threatfeeds.sh
@@ -48,36 +48,36 @@ convert_feed  feed_mwd_domain.tmp feed_mwd_domain.csv ","
 
 echo "Downloading MalwarePatrol Feeds"
 
-wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=clamav_basic" -O feed_mwp_clamav.tmp
+#wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=clamav_basic" -O feed_mwp_clamav.tmp
 
-wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=mozilla_adblock" -O feed_mwp_mozilla.tmp
-echo "INDEX,URL" >> feed_mwp_mozilla.csv
-convert_feed  feed_mwp_mozilla.tmp feed_mwp_mozilla.csv ","
+#wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=mozilla_adblock" -O feed_mwp_mozilla.tmp
+#echo "INDEX,URL" >> feed_mwp_mozilla.csv
+#convert_feed  feed_mwp_mozilla.tmp feed_mwp_mozilla.csv ","
 
-wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=dansguardian" -O feed_mwp_dansguardian.tmp
-echo "INDEX,URL" >> feed_mwp_dansguardian.csv
-convert_feed feed_mwp_dansguardian.tmp feed_mwp_dansguardian.csv ","
+#wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=dansguardian" -O feed_mwp_dansguardian.tmp
+#echo "INDEX,URL" >> feed_mwp_dansguardian.csv
+#convert_feed feed_mwp_dansguardian.tmp feed_mwp_dansguardian.csv ","
 
-wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=mailwasher" -O feed_mwp_mailwasher.tmp
+#wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=mailwasher" -O feed_mwp_mailwasher.tmp
 
-wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=postfix" -O feed_mwp_postfix.tmp
+#wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=postfix" -O feed_mwp_postfix.tmp
 
-wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=smoothwall" -O feed_mwp_smoothwall.tmp
-echo "INDEX,URL" >> feed_mwp_smoothwall.csv
-convert_feed  feed_mwp_smoothwall.tmp feed_mwp_smoothwall.csv ","
+#wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=smoothwall" -O feed_mwp_smoothwall.tmp
+#echo "INDEX,URL" >> feed_mwp_smoothwall.csv
+#convert_feed  feed_mwp_smoothwall.tmp feed_mwp_smoothwall.csv ","
 
-wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=spamassassin" -O feed_mwp_spamassassin.tmp
+#wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=spamassassin" -O feed_mwp_spamassassin.tmp
 
-wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=symantec_smtp_security" -O feed_mwp_symc-smtp.tmp
-echo "INDEX:STATUS:URL" >> feed_mwp_symc-smtp.csv
-convert_feed feed_mwp_symc-smtp.tmp feed_mwp_symc-smtp.csv ":"
+#wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=symantec_smtp_security" -O feed_mwp_symc-smtp.tmp
+#echo "INDEX:STATUS:URL" >> feed_mwp_symc-smtp.csv
+#convert_feed feed_mwp_symc-smtp.tmp feed_mwp_symc-smtp.csv ":"
 
-wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=symantec_websecurity" -O feed_mwp_symc-web.tmp
-echo "INDEX,URL" >> feed_mwp_symc-web.csv
-convert_feed feed_mwp_symc-web.tmp feed_mwp_symc-web.csv ","
+#wget "https://lists.malwarepatrol.net/cgi/getfile?receipt=CHANGE_ME_MWP_PASSWORD&product=8&list=symantec_websecurity" -O feed_mwp_symc-web.tmp
+#echo "INDEX,URL" >> feed_mwp_symc-web.csv
+#convert_feed feed_mwp_symc-web.tmp feed_mwp_symc-web.csv ","
 
-#echo "Downloading SRI Feeds"
-#wget http://mtc.sri.com/live_data/attackers/ -O feed_sri_ip.csv
+echo "Downloading SRI Feeds"
+wget http://mtc.sri.com/live_data/attackers/ -O feed_sri_ip.csv
 
 echo "Downloading TOR Feeds"
 
@@ -88,5 +88,10 @@ convert_feed feed_tor-exit_IP.tmp feed_tor-exit_IP.csv ","
 wget http://torstatus.blutmagie.de/ip_list_all.php/Tor_ip_list_ALL.csv -O feed_tor-all_IP.tmp
 echo "INDEX,IPADDR" >> feed_tor-all_IP.csv
 convert_feed feed_tor-all_IP.tmp feed_tor-all_IP.csv ","
+
+echo "Downloading Emerging Threat Feeds"
+wget https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt -O emerging-Block-IPs.tmp
+echo "INDEX,IPADDR" >> emerging-Block-IPs.csv
+convert_feed emerging-Block-IPs.tmp emerging-Block-IPs.csv ","
 
 echo "Threat Feed Downloads ... Complete!" 
